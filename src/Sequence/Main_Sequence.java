478061a4152251572647431e3e034b72892c1162
jenniferbueso <jenniferbueso@localhost>
2024-02-22 07:28:10 -0600

    Swing: Registro de Usuarios, Iniciar Sesión, Menú Principal

---
+++ src/Sequence/Main_Sequence.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sequence;
+
+import Logic.Users.GestorUsuarios;
+
+/**
+ *
+ * @author jenniferbueso
+ */
+public class Main_Sequence {
+    public static GestorUsuarios gestorUsuarios = new GestorUsuarios();
+    
+    public static void main(String[] args) {
+        PantallaPrincipal pantallaPrincipal = new PantallaPrincipal();
+        pantallaPrincipal.setVisible(true);
+    }
+}
